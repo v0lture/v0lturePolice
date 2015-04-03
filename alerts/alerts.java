7bdbdb6736d3cf98b9982d957a99893f90a3275f
jazzandmax <development@jazzandmax.com>
2015-04-03 11:03:53 -0600

    /alert Handling & DPRT processing
    Added command /alert
    Added handling of /alert <department>
    Added handling of /alert help
    
    yet to add actions to commands

--- alerts/alerts.java
+++ alerts/alerts.java
@@ -3,6 +3,9 @@ package me.jazzandmax.alerts;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -72,6 +75,72 @@ public class alerts extends JavaPlugin{
 		if(getConfig().getString("dprt16").equals("")){
 			this.logger.warning(ChatColor.RED + "[Alerts] Department 15 is undefined");
 		}
+	}
+		
+		// Handle command "/alerts"
+		public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+			Player player = (Player) sender;
+			// Define command
+			if(commandLabel.equalsIgnoreCase("alert")){
+				// just /alert
+				if(args.length == (0)){
+					player.sendMessage(ChatColor.DARK_GREEN + "[Alerts] " + ChatColor.GREEN + "For help type " + ChatColor.GOLD + "/alert help");
+				// /alert help
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("help")){
+					
+				// /alert admin
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("admin")){
+					
+				// /alert 1 (or dprt1)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("1") || args[0].equalsIgnoreCase(getConfig().getString("dprt1"))){
+					
+				// /alert 2 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("2") || args[0].equalsIgnoreCase(getConfig().getString("dprt2"))){
+					
+				// /alert 3 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("3") || args[0].equalsIgnoreCase(getConfig().getString("dprt3"))){
+					
+				// /alert 4 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("4") || args[0].equalsIgnoreCase(getConfig().getString("dprt4"))){
+					
+				// /alert 5 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("5") || args[0].equalsIgnoreCase(getConfig().getString("dprt5"))){
+					
+				// /alert 6 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("6") || args[0].equalsIgnoreCase(getConfig().getString("dprt6"))){
+					
+				// /alert 7 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("7") || args[0].equalsIgnoreCase(getConfig().getString("dprt7"))){
+					
+				// /alert 8 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("8") || args[0].equalsIgnoreCase(getConfig().getString("dprt8"))){
+					
+				// /alert 9 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("9") || args[0].equalsIgnoreCase(getConfig().getString("dprt9"))){
+					
+				// /alert 10 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("10") || args[0].equalsIgnoreCase(getConfig().getString("dprt10"))){
+					
+				// /alert 11 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("11") || args[0].equalsIgnoreCase(getConfig().getString("dprt11"))){
+					
+				// /alert 12 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("12") || args[0].equalsIgnoreCase(getConfig().getString("dprt12"))){
+					
+				// /alert 13 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("13") || args[0].equalsIgnoreCase(getConfig().getString("dprt13"))){
+					
+				// /alert 14 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("14") || args[0].equalsIgnoreCase(getConfig().getString("dprt14"))){
+					
+				// /alert 15 (or dprt2 name)
+				} else if (args.length == (1) && args[0].equalsIgnoreCase("15") || args[0].equalsIgnoreCase(getConfig().getString("dprt15"))){
+					
+				}
+				
+				
+			}
+			return false;
 		
 	}
 }
