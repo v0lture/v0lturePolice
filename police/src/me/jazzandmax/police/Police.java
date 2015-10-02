38056d905fcb38447395d0ccd8da76d57ec0f6f2
OneWithTheCore <ahamilton@linux.com>
2015-10-01 23:44:47 -0400

    Formatting
    Made changes to formatting of police.java to make it easier to read

--- police/src/me/jazzandmax/police/Police.java
+++ police/src/me/jazzandmax/police/Police.java
@@ -45,16 +45,16 @@ public class Police extends JavaPlugin{
 			this.logger.warning(pdfFile.getName() + " has an config issue. Jail location has NOT been set.");
 			getConfig().set("setup", false);
 			Bukkit.broadcast(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Module Error: " + ChatColor.RED + "Module POLICE has a config error, check console for more information.", "jm.police.*");
-		} else if (getConfig().getInt("Y") == (0)) {
+		} 
+		else if (getConfig().getInt("Y") == (0)) {
 			getConfig().set("setup", false);
 			this.logger.warning(pdfFile.getName() + " has an config issue. Jail location is at an unsafe Y axis.");
 			Bukkit.broadcast(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Module Error: " + ChatColor.RED + "Module POLICE has a config error, check console for more information.", "jm.police.*");
-		} else {
+		} 
+		else {
 			getConfig().set("setup", true);
 		}
-		
 	}
-	
 	public Player getPlayer(String name) {
 	    for(Player p : Bukkit.getOnlinePlayers()) {
 	        if(p.getName().equalsIgnoreCase(name))
@@ -62,18 +62,15 @@ public class Police extends JavaPlugin{
 	    }
 	    return null;
 	}
-	
 	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		Player player = (Player) sender;
 		if(commandLabel.equalsIgnoreCase("cop")) {
-			BarAPI.removeBar(player);
 			if(args.length == (0)){
-				player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "Unknown command.");
-				 
-			} else if (args.length == (2) && args[0].equalsIgnoreCase("teleport")){
+				player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "Unknown command."); 
+			} 
+			else if (args.length == (2) && args[0].equalsIgnoreCase("teleport")){
 				if(this.getServer().getPlayer(args[1])!=null){
-					
 					Player targetPlayer = this.getServer().getPlayer(args[1]);
 					Location targetlocation = targetPlayer.getLocation();
 					player.teleport(targetlocation);
@@ -81,27 +78,24 @@ public class Police extends JavaPlugin{
 					BarAPI.setMessage(targetPlayer, ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "A player teleported to you.", 15);
 					targetPlayer.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.RED + player + ChatColor.GREEN + " just teleported to you.");
 					player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "You teleported to " + ChatColor.RED + args[1]);
-				} else {
-					
+				} 
+				else {
 					player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Teleport failed. Player is not online, check spelling and try again.");
 				}
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("teleport") && player.hasPermission("jm.police.tp")){
-				
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("teleport") && player.hasPermission("jm.police.tp")){
 				player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient arguments. Please select a player to teleport to.");
-			
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("teleport") && !player.hasPermission("jm.police.tp")){
-				
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("teleport") && !player.hasPermission("jm.police.tp")){
 				player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient permissions.");
-			
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("freeze") && player.hasPermission("jm.police.freeze")){
-				
-				player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient arguments. Please select a player to freeze.");
-				
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("freeze") && !player.hasPermission("jm.police.freeze")){
-				
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("freeze") && player.hasPermission("jm.police.freeze")){
+				player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient arguments. Please select a player to freeze.");	
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("freeze") && !player.hasPermission("jm.police.freeze")){
 				player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient permissions.");
-				
-			} else if (args.length == (2) && args[0].equalsIgnoreCase("freeze") && player.hasPermission("jm.police.freeze")){
+			} 
+			else if (args.length == (2) && args[0].equalsIgnoreCase("freeze") && player.hasPermission("jm.police.freeze")){
 				if (getPlayer(args[1]) != null){
 					Player targetPlayer = getPlayer(args[1]);
 					resistTries.put((targetPlayer), 3);
@@ -116,11 +110,12 @@ public class Police extends JavaPlugin{
 					player.sendMessage(ChatColor.GOLD + "[Police] [WARNING] " + ChatColor.DARK_RED + args[1] + ChatColor.GREEN + " can try to unfreeze at anytime by typing " + ChatColor.GOLD + "/cop resist");
 					targetPlayer.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "You have just been frozen by " + ChatColor.GOLD + player + ChatColor.GREEN + ".");
 					targetPlayer.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "You can try to resist this by typing " + ChatColor.GOLD + "/cop resist" + ChatColor.GREEN + ". The cop can freeze you again if you resist!");
-				} else {
-					
+				} 
+				else {
 					player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Freeze failed. Player is not online, check spelling and try again.");
 				}
-			} else if (args.length == (2) && args[0].equalsIgnoreCase("unfreeze") && player.hasPermission("jm.police.freeze")){
+			} 
+			else if (args.length == (2) && args[0].equalsIgnoreCase("unfreeze") && player.hasPermission("jm.police.freeze")){
 				if (getPlayer(args[1]) != null){
 					Player targetPlayer = getPlayer(args[1]);
 					targetPlayer.removePotionEffect(PotionEffectType.BLINDNESS);
@@ -132,7 +127,8 @@ public class Police extends JavaPlugin{
 					targetPlayer.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "You have been unfrozen by the police");
 					player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "You unfroze " + ChatColor.RED + args[1]);
 				}
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("resist") && player.hasPermission("jm.police.resist") && player.hasPotionEffect(PotionEffectType.BLINDNESS)){
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("resist") && player.hasPermission("jm.police.resist") && player.hasPotionEffect(PotionEffectType.BLINDNESS)){
 				rand = new Random().nextDouble();
 				resistOdds = rand;
 				if (resistOdds >= .0 && resistOdds <= .20 && resistTries.get((player)) <= 3 && resistTries.get((player)) > 0) {
@@ -145,25 +141,20 @@ public class Police extends JavaPlugin{
 					player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "You have resisted the freeze, the police may freeze you again.");
 				}
 				else if (resistTries.get(player) <= 0) {
-					BarAPI.removeBar(player);
-					BarAPI.setMessage(player, ChatColor.RED + "SORRY! You are out of resist attempts.", 10);
 					player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "You are out of resist tries.");
 				}
 				else {
 					resistTries.put((player), (resistTries.get((player)) - 1));
-					BarAPI.removeBar(player);
-					BarAPI.setMessage(player, ChatColor.RED + "Resist failed. You have " + resistTries.get(player) + " tries left.", 10);
 					player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "Resist failed. You have " + (resistTries.get(player)) + " tries left");
 				}
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("resist") && player.hasPermission("jm.police.resist") && !player.hasPotionEffect(PotionEffectType.BLINDNESS)){
-				
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("resist") && player.hasPermission("jm.police.resist") && !player.hasPotionEffect(PotionEffectType.BLINDNESS)){
 				player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Resist failed. You were not frozen or it has expired.");
-			
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("resist") && !player.hasPermission("jm.police.resist")){
-				
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("resist") && !player.hasPermission("jm.police.resist")){
 				player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] "+ ChatColor.DARK_RED + "Error:  " + ChatColor.RED + "Insufficient permissions.");
-				
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("arrest") && player.hasPermission("jm.police.arrest")){
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("arrest") && player.hasPermission("jm.police.arrest")){
 				if (getConfig().getBoolean("setup") == false){
 					player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "The config.yml is not setup properly for this feature.");
 				} 
@@ -173,13 +164,11 @@ public class Police extends JavaPlugin{
 				else {
 					player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient arguments. Please select a player to arrest.");
 				}
-				
-			} else if (args.length == (2) && args[0].equalsIgnoreCase("arrest") && player.hasPermission("jm.police.arrest")){
-			
+			} 
+			else if (args.length == (2) && args[0].equalsIgnoreCase("arrest") && player.hasPermission("jm.police.arrest")){
 				if(getConfig().getBoolean("setup") == false){
-					
-				} else {
-					
+				} 
+				else {
 						Player targetPlayer = getPlayer(args[1]);
 						targetPlayer.setGameMode(GameMode.ADVENTURE);
 						World map = player.getWorld();
@@ -191,10 +180,11 @@ public class Police extends JavaPlugin{
 						player.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GREEN + "You sent " + ChatColor.RED + args[1] + ChatColor.GREEN +  " to jail.");
 						targetPlayer.sendMessage(ChatColor.DARK_GREEN + "[Police] " + ChatColor.GOLD + "You were sent to jail by " + ChatColor.RED + player + ChatColor.GOLD + ".");
 				}
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("unfreeze")){
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("unfreeze")){
 				player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient arguments. Please select a player to unfreeze.");
-				
-			} else if (args.length == (1) && args[0].equalsIgnoreCase("setjail")){
+			} 
+			else if (args.length == (1) && args[0].equalsIgnoreCase("setjail")){
 				Location newjail = player.getLocation();
 				getConfig().set("X", newjail.getBlockX());
 				getConfig().set("Y", newjail.getBlockY());
@@ -204,15 +194,17 @@ public class Police extends JavaPlugin{
 				player.sendMessage(ChatColor.DARK_GREEN + "[Y] " + ChatColor.GREEN + newjail.getBlockY());
 				player.sendMessage(ChatColor.DARK_GREEN + "[Z] " + ChatColor.GREEN + newjail.getBlockZ());
 				saveConfig();
-				
-			} else if (args.length >= (0) && args[0].equalsIgnoreCase("arrest") && !player.hasPermission("jm.police.arrest")){
-				player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient permissions.");
-				
-			} else if (!(args.length == (0)) || !(args.length == (1)) || !(args.length == (2))) {
+			} 
+			else if (args.length >= (0) && args[0].equalsIgnoreCase("arrest") && !player.hasPermission("jm.police.arrest")){
+				player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Insufficient permissions.");				
+			}
+			else if (!(args.length == (0)) || !(args.length == (1)) || !(args.length == (2))) {
 				player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Too many arguments.");
-			} else if (args.length >= (0)){
+			} 
+			else if (args.length >= (0)){
 				player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Error: " + ChatColor.RED + "Command not found");
-		} else {
+		} 
+		else {
 			player.sendMessage(ChatColor.DARK_GREEN + "[JMPS] " + ChatColor.DARK_RED + "Sorry! " + ChatColor.RED + "Unhandled action.");
 		}
 		}
